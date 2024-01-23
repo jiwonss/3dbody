a68e54369b066118efc8489a73e756d58d891ae7
서지원 <sjs3326@naver.com>
2024-01-23 14:29:30 +0900

    feat: BaseEntity 추가

---
+++ backend/src/main/java/com/ssafy/backend/global/entity/BaseEntity.java
@@ -0,0 +1,17 @@
+package com.ssafy.backend.global.entity;
+
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
