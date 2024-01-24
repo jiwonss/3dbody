d28c49d81b5d2ad86a24cf1862239957108e4a68
이승호 <kials1@naver.com>
2024-01-24 13:54:20 +0900

    vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        강성권 잔다
+      </div>
+    </>
+  )
+}
+
+export default App
