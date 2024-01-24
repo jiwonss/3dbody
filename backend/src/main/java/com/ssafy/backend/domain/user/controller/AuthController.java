c2d9f5e04ac12c84b33774bc2d33d8f804765255
서지원 <sjs3326@naver.com>
2024-01-25 01:16:57 +0900

    feat: login 구현(진행중)

--- backend/src/main/java/com/ssafy/backend/domain/user/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/controller/AuthController.java
@@ -1,7 +1,14 @@
 package com.ssafy.backend.domain.user.controller;
 
+import com.ssafy.backend.domain.user.dto.LoginRequestDto;
 import com.ssafy.backend.domain.user.dto.SignupRequestDto;
 import com.ssafy.backend.domain.user.service.AuthService;
+import com.ssafy.backend.global.dto.Response;
+import com.ssafy.backend.global.jwt.dto.TokenDto;
+import com.ssafy.backend.global.jwt.dto.UserInfoDto;
+import com.ssafy.backend.global.jwt.service.JwtService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,11 +22,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final JwtService jwtService;
 
     @PostMapping("/signup")
-    public ResponseEntity signup(@RequestBody SignupRequestDto signupRequestDto) throws Exception {
+    public ResponseEntity signup(@RequestBody SignupRequestDto signupRequestDto) {
         authService.signup(signupRequestDto);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(Response.success());
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        UserInfoDto userInfoDto = authService.login(loginRequestDto.getEmail(), loginRequestDto.getPassword());
+        TokenDto tokenDto = jwtService.issueToken(userInfoDto);
+
+        Cookie accessTokenCookie = new Cookie("accessToken", tokenDto.getAccessToken());
+        accessTokenCookie.setMaxAge((int)tokenDto.getAccessTokenExpired());
+        accessTokenCookie.setPath("/");
+        Cookie refreshTokenCookie = new Cookie("refreshToken", tokenDto.getRefreshToken());
+        refreshTokenCookie.setMaxAge((int)tokenDto.getRefreshTokenExpired());
+        refreshTokenCookie.setPath("/");
+        response.addCookie(accessTokenCookie);
+        response.addCookie(refreshTokenCookie);
+
+        return ResponseEntity.ok(Response.success(tokenDto));
     }
 
 }
--- backend/src/main/java/com/ssafy/backend/domain/user/service/AuthService.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/service/AuthService.java
@@ -2,11 +2,15 @@ package com.ssafy.backend.domain.user.service;
 
 import com.ssafy.backend.domain.user.dto.SignupRequestDto;
 import com.ssafy.backend.domain.user.entity.User;
+import com.ssafy.backend.global.jwt.dto.TokenDto;
+import com.ssafy.backend.global.jwt.dto.UserInfoDto;
 
 import java.util.Optional;
 
 public interface AuthService {
 
-    void signup(SignupRequestDto signupRequestDto) throws Exception;
+    void signup(SignupRequestDto signupRequestDto);
+    UserInfoDto login(String email, String password);
+    TokenDto reissue(String refreshToken);
 
 }
