e5b1a99ba4b9cc452014ecad5432e80d9644729f
seonggwon <rkdtjdrnjs11@naver.com>
2024-01-25 14:41:50 +0900

    chore/tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import BackButton from './common/BackButton'
 
 function App() {
+<<<<<<< PATCH SET (629f6c chore/tailwind)
+=======
 
+>>>>>>> BASE      (1c0d2c Merge "test" into feat/fe/common)
   return (
     <div>
+<<<<<<< PATCH SET (629f6c chore/tailwind)
+      <BackButton />
+=======
 
+>>>>>>> BASE      (1c0d2c Merge "test" into feat/fe/common)
     </div>
   )
 }
