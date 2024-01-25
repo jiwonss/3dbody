07ee989245539c750c5a3ddcdf3ac4c9a78cd20e
김민욱 <minwookkim@naver.com>
2024-01-25 19:56:08 +0900

    feat: 토글탭 컴포넌트

--- frontend/src/common/ToggleTap.jsx
+++ frontend/src/common/ToggleTap.jsx
@@ -1,10 +1,36 @@
 import Button from "./Button";
+import { useState } from "react";
 
 const ToggleTap = () => {
+  const [isLeftSelected, SetisLeftSelected] = useState(true);
+  const [isRightSelected, SetisRightSelected] = useState(false);
+
+  const onClick = () => {
+    SetisLeftSelected(!isLeftSelected)
+    SetisRightSelected(!isRightSelected)
+    console.log(isLeftSelected)
+    console.log(isRightSelected)
+  }
+
+  let leftCheck = 'small'
+  let rightCheck = 'large'
+
+  if (isLeftSelected) {
+    leftCheck = 'small'
+  } else {
+    leftCheck = 'large'
+  }
+
+  if (isRightSelected) {
+    rightCheck = 'small'
+  } else {
+    rightCheck = 'large'
+  }
+
   return (
     <div>
-      <Button buttonStyle="small" buttonName="캘린더" />
-      <Button buttonStyle="large" buttonName="그래프" />
+      <Button buttonStyle={leftCheck} buttonName="캘린더" onClick={onClick}/>
+      <Button buttonStyle={rightCheck} buttonName="그래프" onClick={onClick}/>
     </div>
   );
 };
