e1e02ef871bc56a068df43565a40eb0e36199212
김민욱 <minwookkim@naver.com>
2024-01-25 14:55:46 +0900

    clean

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,10 @@
 import './App.css'
-import BackButton from './common/BackButton'
 
 function App() {
-<<<<<<< PATCH SET (629f6c chore/tailwind)
-=======
-
->>>>>>> BASE      (1c0d2c Merge "test" into feat/fe/common)
   return (
     <div>
-<<<<<<< PATCH SET (629f6c chore/tailwind)
-      <BackButton />
-=======
-
->>>>>>> BASE      (1c0d2c Merge "test" into feat/fe/common)
     </div>
   )
 }
 
-export default App
+export default App;
