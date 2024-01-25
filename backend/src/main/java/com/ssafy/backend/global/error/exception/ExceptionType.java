434abe89df5878fad4dc5162f2d0396b1302d0de
서지원 <sjs3326@naver.com>
2024-01-25 16:21:08 +0900

    fix: ExceptionType 수정

--- backend/src/main/java/com/ssafy/backend/global/error/exception/ExceptionType.java
+++ backend/src/main/java/com/ssafy/backend/global/error/exception/ExceptionType.java
@@ -19,7 +19,7 @@ public enum ExceptionType {
     CERTIFICATION_EXCEPTION(HttpStatus.UNAUTHORIZED, "자격 증명이 되어 있지 않습니다."),
 
     FORBIDDEN_EXCEPTION(HttpStatus.FORBIDDEN, "권한이 없습니다."),
-    Authentication_EXCEPTION(HttpStatus.UNAUTHORIZED, "인증되지 않은 요청입니다."),
+    AUTHENTICATION_EXCEPTION(HttpStatus.UNAUTHORIZED, "인증되지 않은 요청입니다."),
     MAIL_SEND_FAILED_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "FAILED_TO_SEND_MAIL");
 
     private final HttpStatus httpStatus;
