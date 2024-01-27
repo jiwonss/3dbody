7b6ed84f14cc4d061f54f35a0cacb3cace0b717c
김태훈 <xogns5@naver.com>
2024-01-27 16:34:29 +0900

    fix:jwt구조수정

--- backend/src/main/java/com/ssafy/backend/BackendApplication.java
+++ backend/src/main/java/com/ssafy/backend/BackendApplication.java
@@ -2,10 +2,12 @@ package com.ssafy.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@ComponentScan(basePackages = "com.ssafy.backend")
 public class BackendApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/ssafy/backend/global/jwt/service/JwtServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/global/jwt/service/JwtServiceImpl.java
@@ -22,12 +22,21 @@ import static com.ssafy.backend.global.error.exception.ExceptionType.INVALID_TOK
 import static javax.management.timer.Timer.ONE_MINUTE;
 
 @Service
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class JwtServiceImpl implements JwtService {
 
-    @Autowired
+
     private final JwtUtils jwtUtils;
+
+    @Autowired
+    public JwtServiceImpl(JwtUtils jwtUtils, TokenRepository tokenRepository){
+        this.jwtUtils = jwtUtils;
+        this.tokenRepository = tokenRepository;
+    }
+
+    @Autowired
     private final TokenRepository tokenRepository;
+
 
     private static final String CLAIM_EMAIL = "email";
     private static final String CLAIM_NICKNAME = "nickname";
