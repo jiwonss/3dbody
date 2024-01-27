a7c0d31039c1661dbba1371f09be29ee9b422d0a
김태훈 <xogns5@naver.com>
2024-01-27 18:38:04 +0900

    db서버연결추가

--- backend/src/main/java/com/ssafy/backend/domain/challenge/repository/UserChallengeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.backend.domain.challenge.repository;
-
-import com.ssafy.backend.domain.challenge.entity.UserChallenge;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserChallengeRepository extends JpaRepository<UserChallenge, Long>, UserChallengeCustomRepository {
-}
