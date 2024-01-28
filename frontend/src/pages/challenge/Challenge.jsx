1bf8578cedf5b51c6cc2ceb48d1d685034c37865
김민욱 <minwookkim@naver.com>
2024-01-28 14:41:04 +0900

    feat: 챌린지 카드 만드는 중

---
+++ frontend/src/pages/challenge/Challenge.jsx
@@ -0,0 +1,11 @@
+import ChallengeCard from './../../components/challenge/ChallengeCard';
+
+const Challenge = () => {
+  return (
+    <div>
+      <ChallengeCard />
+    </div>
+  );
+};
+
+export default Challenge;
