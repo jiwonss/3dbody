d3d2745f144bd4a719a09aba27d4fd5eaf8a6620
박형규 <gudrb4869@naver.com>
2024-01-29 10:16:14 +0900

    feat: 챌린지 도메인 및 api 추가 - 진행중인 챌린지 - 참여중인 챌린지 - 챌린지 상세 조회 - 챌린지 정보 등록

---
+++ backend/src/main/java/com/ssafy/backend/domain/challenge/entity/Challenge.java
@@ -0,0 +1,50 @@
+package com.ssafy.backend.domain.challenge.entity;
+
+import com.ssafy.backend.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(of = {"challengeId"}, callSuper = false)
+@NamedEntityGraph
+public class Challenge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long challengeId; // 챌린지ID, BIGINT, Primary Key
+
+     @ManyToOne
+     @JoinColumn(name = "user_id")
+     private User user; // 작성자ID, BIGINT, Foreign Key, NOT NULL
+
+    @Column(nullable = false)
+    private String title; // 챌린지이름, VARCHAR(100), NOT NULL
+
+    private String content; // 내용, TEXT, NOT NULL
+
+    private String thumbnail; // 썸네일, VARCHAR(255)
+
+    private String image; // 이미지, VARCHAR(255)
+
+    @ColumnDefault("0")
+    private int entry = 0; // 참가자수, INT, default(0)
+
+    @ColumnDefault("0")
+    private int hit = 0; // 조회수, INT, default(0)
+
+    @Enumerated(EnumType.STRING)
+    private Status status; // 상태, ENUM, NOT NULL
+
+    private LocalDateTime startDate; // 시작날짜, TIMESTAMP NOT NULL
+
+    private LocalDateTime endDate; // 종료날짜, TIMESTAMP NOT NULL
+
+}
