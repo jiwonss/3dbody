337cac93bdd40c5710603287c1414c2714a1f71f
박형규 <gudrb4869@naver.com>
2024-01-29 16:11:12 +0900

    fix: 참여가능 챌린지 목록, 종료된 챌린지 목록 쿼리 오류 수정

--- backend/src/main/java/com/ssafy/backend/domain/challenge/repository/ChallengeRepository.java
+++ backend/src/main/java/com/ssafy/backend/domain/challenge/repository/ChallengeRepository.java
@@ -12,13 +12,13 @@ import java.util.List;
 public interface ChallengeRepository extends JpaRepository<Challenge, Long>, ChallengeCustomRepository {
 
     // 참여가능 챌린지 목록
-    @Query("select new com.ssafy.backend.domain.challenge.dto.ChallengeListResponseDto(c.challengeId, c.title, c.thumbnail, c.entry, c.hit, c.user.userId, c.user.nickname) " +
+    @Query("select new com.ssafy.backend.domain.challenge.dto.ChallengeListResponseDto(c.challengeId, c.title, c.summary, c.thumbnail, c.entry, c.hit, c.user.userId, c.user.nickname, c.startDate, c.endDate) " +
             "from Challenge c inner join c.user u " +
             "where c.endDate > :now")
     List<ChallengeListResponseDto> findAllByEndDateIsAfter(@Param("now") LocalDateTime now);
 
     // 종료된 챌린지 목록
-    @Query("select new com.ssafy.backend.domain.challenge.dto.ChallengeListResponseDto(c.challengeId, c.title, c.thumbnail, c.entry, c.hit, c.user.userId, c.user.nickname) " +
+    @Query("select new com.ssafy.backend.domain.challenge.dto.ChallengeListResponseDto(c.challengeId, c.title, c.summary, c.thumbnail, c.entry, c.hit, c.user.userId, c.user.nickname, c.startDate, c.endDate) " +
             "from Challenge c inner join c.user u " +
             "where c.endDate < :now")
     List<ChallengeListResponseDto> findAllByEndDateIsBefore(@Param("now") LocalDateTime now);
