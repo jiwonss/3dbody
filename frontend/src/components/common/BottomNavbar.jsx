c80d22fe6947bdbf417ad24065b05573ab4b90b1
이승호 <kials1@naver.com>
2024-02-01 08:54:25 +0900

    Feat: 바텀 네브링크로 수정

--- frontend/src/components/common/BottomNavbar.jsx
+++ frontend/src/components/common/BottomNavbar.jsx
@@ -1,31 +1,23 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { CalendarDaysIcon, Cog6ToothIcon, HomeIcon, TrophyIcon } from "@heroicons/react/24/solid";
-import { useRecoilState } from 'recoil';
-import { bottomNavState } from '../../recoil/common/BottomNavState';
 
 const BottomNavbar = () => {
-  const [isSelected, setIsSelected] = useRecoilState(bottomNavState)
-
-  const onClickSelected = (page) => {
-    setIsSelected(page)
-  }
-
   return (
     <>
     <div className="fixed bottom-0 left-0 right-0">
       <div className='flex justify-between p-4 bg-white border-2'>
-        <Link to="/" onClick={() => onClickSelected("")}>
-          <HomeIcon className={ `w-6 h-6 ${isSelected === "" ? 'text-green-700' : 'text-red-700'}`}/>
-        </Link>
-        <Link to="/diary" onClick={() => onClickSelected("diary")}>
-          <CalendarDaysIcon className={ `w-6 h-6 ${isSelected === "diary" ? 'text-green-700' : 'text-red-700'}`}/>
-        </Link>
-        <Link to="/challenge" onClick={() => onClickSelected("challenge")}>
-          <TrophyIcon className={ `w-6 h-6 ${isSelected === "challenge" ? 'text-green-700' : 'text-red-700'}`}/>
-        </Link>
-        <Link to="/mypage" onClick={() => onClickSelected("mypage")}>
-          <Cog6ToothIcon className={ `w-6 h-6 ${isSelected === "mypage" ? 'text-green-700' : 'text-red-700'}`}/>
-        </Link>
+        <NavLink to="/">
+            <HomeIcon className={`w-6 h-6`} />
+        </NavLink>
+        <NavLink to="/diary">
+          <CalendarDaysIcon className={ `w-6 h-6 `}/>
+        </NavLink>
+        <NavLink to="/challenge">
+          <TrophyIcon className={ `w-6 h-6 `}/>
+        </NavLink>
+        <NavLink to="/mypage">
+          <Cog6ToothIcon className={ `w-6 h-6 `}/>
+        </NavLink>
       </div>
     </div>
       <Outlet />
