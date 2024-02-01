22dd02eba5ab1546fda8e5f747fd8a95641def19
김민욱 <minwookkim@naver.com>
2024-02-01 17:43:12 +0900

    Feat : 챌린지 등록완료, S3에 이미지 올리기 진행중

--- frontend/src/pages/challenge/ChallengePage.jsx
+++ frontend/src/pages/challenge/ChallengePage.jsx
@@ -1,18 +1,37 @@
 import ToggleTap from "../../components/common/ToggleTap";
 import PageTitle from "../../components/common/PageTitle";
-import ChallengeParticipating from './ChallengeParticipatingPage';
-import { useRecoilValue } from 'recoil';
-import { toggleState } from '../../recoil/common/ToggleState';
-import ChallengeCanParticipate from './CallengeCanParticipatePage';
-
+import ChallengeParticipating from "./ChallengeParticipatingPage";
+import { useRecoilValue } from "recoil";
+import { toggleState } from "../../recoil/common/ToggleState";
+import ChallengeCanParticipate from "./CallengeCanParticipatePage";
+import Button from "./../../components/common/Button";
+import { userState } from "../../recoil/common/UserState";
+import { Link } from 'react-router-dom';
 
 const Challenge = () => {
-  const isSelected = useRecoilValue(toggleState)
+  const isSelected = useRecoilValue(toggleState);
+  const user = useRecoilValue(userState);
+
   return (
     <div>
       <PageTitle pageTitle={"챌린지"} />
-      <ToggleTap leftTitle={"참여 가능"} rightTitle={"참여 중"} state={toggleState} />
-      { isSelected === "left" ?  <ChallengeCanParticipate /> : <ChallengeParticipating />}
+      <ToggleTap
+        leftTitle={"참여 가능"}
+        rightTitle={"참여 중"}
+        state={toggleState}
+      />
+      {isSelected === "left" ? (
+        <ChallengeCanParticipate />
+      ) : (
+        <ChallengeParticipating />
+      )}
+      <Link to="/challenge/registration">
+        <div>
+          {user.info.role === "ROLE_ADMIN" ? (
+            <Button buttonName={"챌린지 생성하기"} />
+          ) : null}
+        </div>
+      </Link>
     </div>
   );
 };
