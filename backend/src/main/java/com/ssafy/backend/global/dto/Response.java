20aa73c8e9742a01efaadc1a3cc41f63332fea18
서지원 <sjs3326@naver.com>
2024-02-02 13:33:10 +0900

    refactor: Response 불필요한 import 제거

--- backend/src/main/java/com/ssafy/backend/global/dto/Response.java
+++ backend/src/main/java/com/ssafy/backend/global/dto/Response.java
@@ -1,6 +1,5 @@
 package com.ssafy.backend.global.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
