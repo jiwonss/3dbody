f3bb659327bdc28485b041f948b1a5b0682195d1
seonggwon <rkdtjdrnjs11@naver.com>
2024-02-02 09:02:39 +0900

    전역 모달 설정

--- frontend/src/recoil/modal/ModalManager.jsx
+++ frontend/src/recoil/modal/ModalManager.jsx
@@ -9,7 +9,7 @@ const ModalManager = () => {
   const closeModal = () => {
     setModalName(null);
   };
-
+  // 화면에 띄울 모달 설정 recoil에 이름으로 저장 해서 해당 이름에 맞는 작성한 모달 컴포넌트 호출
   switch (modalName) {
     case "changeNickname":
       return <ChangeNicknameModal onClose={closeModal} />;
