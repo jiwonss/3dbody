77a01b65baf24344fb663c8183193674be43074d
서지원 <sjs3326@naver.com>
2024-02-03 23:16:46 +0900

    refactor: exception 수정 GlobalExceptionAdvice의 UserException 수정 및 InbodyException 추가 ExceptionType에 WITHDRAW_USER 추가

--- backend/src/main/java/com/ssafy/backend/global/error/exception/ExceptionType.java
+++ backend/src/main/java/com/ssafy/backend/global/error/exception/ExceptionType.java
@@ -8,17 +8,17 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ExceptionType {
 
-    DUPLICATED_USER(HttpStatus.BAD_REQUEST, "이미 존재 하는 회원 입니다."),
-    DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일 입니다."),
-    DUPLICATED_NICKNAME(HttpStatus.BAD_REQUEST, "이미 존재 하는 닉네임 입니다."),
+    DUPLICATED_USER(HttpStatus.BAD_REQUEST, "이미 존재하는 회원입니다."),
+    DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다."),
+    DUPLICATED_NICKNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 닉네임입니다."),
     INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "잘못된 비밀번호 입니다."),
     INVALID_PIN(HttpStatus.BAD_REQUEST, "잘못된 PIN 입니다."),
     INVALID_EMAIL(HttpStatus.BAD_REQUEST, "email을 다시 확인해 주세요"),
-    INVALID_USER(HttpStatus.BAD_REQUEST, "존재하지 않는 사용자입니다."),
-    WITHDRWA_USER(HttpStatus.BAD_REQUEST, "회원탈퇴한 사용자입니다."),
+    INVALID_USER(HttpStatus.BAD_REQUEST, "존재하지않는 사용자입니다."),
+    WITHDRAW_USER(HttpStatus.BAD_REQUEST, "회원탈퇴한 사용자입니다."),
 
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "사용할 수 없는 토큰 입니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "사용할 수 없는 토큰입니다."),
 
     CERTIFICATION_EXCEPTION(HttpStatus.UNAUTHORIZED, "자격 증명이 되어 있지 않습니다."),
 
