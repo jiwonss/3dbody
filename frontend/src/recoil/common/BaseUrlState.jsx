19696b55777b3a69d042a142a71e928c3e47c7aa
김태훈 <xogns5@naver.com>
2024-02-04 20:39:17 +0900

    fix: baseurl수정

--- frontend/src/recoil/common/BaseUrlState.jsx
+++ frontend/src/recoil/common/BaseUrlState.jsx
@@ -2,7 +2,7 @@ import { atom } from "recoil";
 
 const baseUrlState = atom({
   key: "baseUrlState",
-  default: "http://i10c204.p.ssafy.io:8082/",
+  default: "https://i10c204.p.ssafy.io/",
 });
 
 export { baseUrlState };
