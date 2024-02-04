c19c610e210fec24f16e8f5cc9402a04b688fe30
서지원 <sjs3326@naver.com>
2024-02-04 20:22:22 +0900

    refactor: ExceptionType 메시지 수정

--- backend/src/main/java/com/ssafy/backend/global/error/exception/ExceptionType.java
+++ backend/src/main/java/com/ssafy/backend/global/error/exception/ExceptionType.java
@@ -14,7 +14,7 @@ public enum ExceptionType {
     INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "잘못된 비밀번호 입니다."),
     INVALID_PIN(HttpStatus.BAD_REQUEST, "잘못된 PIN 입니다."),
     INVALID_EMAIL(HttpStatus.BAD_REQUEST, "email을 다시 확인해 주세요"),
-    INVALID_USER(HttpStatus.BAD_REQUEST, "존재하지않는 사용자입니다."),
+    INVALID_USER(HttpStatus.BAD_REQUEST, "존재하지 않는 사용자입니다."),
     INVALID_VALUE(HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다."),
     WITHDRAW_USER(HttpStatus.BAD_REQUEST, "회원탈퇴한 사용자입니다."),
 
@@ -27,7 +27,7 @@ public enum ExceptionType {
     AUTHENTICATION_EXCEPTION(HttpStatus.UNAUTHORIZED, "인증되지 않은 요청입니다."),
     MAIL_SEND_FAILED_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "FAILED_TO_SEND_MAIL"),
 
-    INVALID_INBODY(HttpStatus.BAD_REQUEST, "존재하는 인바디 정보입니다."),
+    INVALID_INBODY(HttpStatus.BAD_REQUEST, "존재하지 않는 인바디 정보입니다."),
 
     INVALID_FILE(HttpStatus.BAD_REQUEST, "존재하지 않는 파일입니다.");
 
