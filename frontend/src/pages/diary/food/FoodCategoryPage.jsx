88199e9279790224117cf2161c5a8cdec00d37b0
seungho <kials1@naver.com>
2024-02-05 22:42:00 +0900

    Feat: 음식 단위 표시

--- frontend/src/pages/diary/food/FoodCategoryPage.jsx
+++ frontend/src/pages/diary/food/FoodCategoryPage.jsx
@@ -1,43 +1,47 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { useRecoilValue } from "recoil";
+import { useEffect } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { ChevronLeftIcon, PlusCircleIcon } from "@heroicons/react/24/solid";
 import { BsFillTrash3Fill } from "react-icons/bs";
 import { baseUrlState } from "../../../recoil/common/BaseUrlState";
 import { userState } from "../../../recoil/common/UserState";
 import { selectedDateState } from "../../../recoil/diary/SelectedDateState";
+import { userCategoryFoodState } from "../../../recoil/diary/UserCategoryFoodState";
 import PageTitle from "../../../components/common/PageTitle";
 import Description from "../../../components/diary/training/Description";
+import FoodDetailBox from "../../../components/diary/food/FoodDetailBox";
 
 const FoodCategoryPage = () => {
   const baseUrl = useRecoilValue(baseUrlState);
   const user = useRecoilValue(userState);
   const selectedDate = useRecoilValue(selectedDateState);
-  const [foodList, setFoodList] = useState([]);
+  const [userCategoryFood, setUserCategoryFood] = useRecoilState(
+    userCategoryFoodState
+  );
   const navigate = useNavigate();
   const { category } = useParams();
 
   // 날짜별 칼탄단지 총합 --------------------------------------------------------------------
-  const totalcalorie = foodList
+  const totalcalorie = userCategoryFood
     .reduce((acc, cur) => {
       return acc + cur.food.calorie;
     }, 0)
     .toFixed(1);
 
-  const totalcarbohydrate = foodList
+  const totalcarbohydrate = userCategoryFood
     .reduce((acc, cur) => {
       return acc + cur.food.carbohydrate;
     }, 0)
     .toFixed(1);
 
-  const totalprotein = foodList
+  const totalprotein = userCategoryFood
     .reduce((acc, cur) => {
       return acc + cur.food.protein;
     }, 0)
     .toFixed(1);
 
-  const totallipid = foodList
+  const totallipid = userCategoryFood
     .reduce((acc, cur) => {
       return acc + cur.food.lipid;
     }, 0)
@@ -46,7 +50,9 @@ const FoodCategoryPage = () => {
 
   // 뒤로가기 버튼
   const onClickBackBtn = () => {
-    navigate(`/diary/food/${selectedDate[0]}/${selectedDate[1]}/${selectedDate[2]}`);
+    navigate(
+      `/diary/food/${selectedDate[0]}/${selectedDate[1]}/${selectedDate[2]}`
+    );
   };
 
   // 식단 카테고리별 데이터 가져오기
@@ -55,18 +61,20 @@ const FoodCategoryPage = () => {
       .get(
         `${baseUrl}api/management/food/list/category/${user.info.userId}?year=${
           selectedDate[0]
-        }&month=${selectedDate[1]}&day=${selectedDate[2]}&category=${category.substring(0, 2)}`
+        }&month=${selectedDate[1]}&day=${
+          selectedDate[2]
+        }&category=${category.substring(0, 2)}`
       )
       .then((res) => {
         console.log(res.data);
-        setFoodList(res.data);
+        setUserCategoryFood(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
-  // 삭제 버튼
+  // // 삭제 버튼
   const onClickDelete = async (foodId) => {
     await axios
       .delete(`${baseUrl}api/management/food/delete/${foodId}`)
@@ -84,20 +92,14 @@ const FoodCategoryPage = () => {
     getUserFoodCategory();
   }, []);
 
-  const onClickBtn = (params) => {
-    if (params === "left") {
-      setSelectedBtn("left");
-    } else {
-      setSelectedBtn("right");
-    }
-    console.log(selectedBtn);
-  };
-
   return (
     <>
       <div className="sticky top-0 bg-white">
         <div className="absolute my-4">
-          <ChevronLeftIcon className="w-6 h-6 text-blue-500" onClick={onClickBackBtn} />
+          <ChevronLeftIcon
+            className="w-6 h-6 text-blue-500"
+            onClick={onClickBackBtn}
+          />
         </div>
         <PageTitle pageTitle={category} />
         <div className="p-4">
@@ -144,48 +146,29 @@ const FoodCategoryPage = () => {
       </div>
       <div className="mx-4">
         {/* 카테고리 - 음식 리스트 */}
-        {foodList.map((data) => {
+        {userCategoryFood.map((data) => {
           return (
             <div
-              className="flex items-center mb-2 border-2 border-gray-400 rounded-md"
+              className="flex mb-2 border-2 border-gray-400 rounded-md"
               key={data.userFoodId}
             >
               {/* 음식 정보 */}
-              <div className="flex flex-col pl-4 basis-10/12">
-                <p>{data.food.name}</p>
+              <div className="flex flex-col justify-center pb-1 pl-4 basis-full">
+                <p className="text-base">{data.food.name}</p>
                 <p style={{ fontSize: "12px" }}>
-                  탄 {data.food.carbohydrate.toFixed(1)}g 단 {data.food.protein.toFixed(1)}g 지{" "}
+                  탄 {data.food.carbohydrate.toFixed(1)}g 단{" "}
+                  {data.food.protein.toFixed(1)}g 지{" "}
                   {data.food.lipid.toFixed(1)}g
                 </p>
               </div>
               {/* 정보 변경 및 삭제 */}
-              <div className="flex flex-col">
-                <div className="flex gap-2 mt-2 mr-2">
-                  <button
-                    className="text-xs text-white bg-teal-700 rounded-full basis-1/2"
-                    onClick={() => onClickBtn("left")}
-                  >
-                    g 단위
-                  </button>
-                  <button
-                    className="text-xs text-white bg-teal-700 rounded-full basis-1/2"
-                    onClick={() => onClickBtn("right")}
-                  >
-                    개수 단위
-                  </button>
-                </div>
-                <div className="flex basis-2/5">
-                  <input
-                    type="text"
-                    onChange={(e) => {
-                      console.log(e.target.value);
-                    }}
-                    value={data.food.servingSize + " g"}
-                    className="m-2 text-center border-2 basis-1/2"
-                  />
-                  <div className="flex items-center" onClick={() => onClickDelete(data.userFoodId)}>
-                    <BsFillTrash3Fill className="w-6 h-6" />
-                  </div>
+              <div className="flex justify-end gap-3 ml-8 mr-4">
+                <FoodDetailBox data={data} category={category} />
+                <div
+                  className="flex items-center"
+                  onClick={() => onClickDelete(data.userFoodId)}
+                >
+                  <BsFillTrash3Fill className="w-6 h-6" />
                 </div>
               </div>
             </div>
