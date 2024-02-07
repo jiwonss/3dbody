818ec09436e6fb47e8b5e7385887df213fb14b44
김태훈 <xogns5@naver.com>
2024-02-07 11:10:33 +0900

    feat: 루틴운동,세트추가삭제

--- backend/src/main/java/com/ssafy/backend/domain/routine/service/RoutineService.java
+++ backend/src/main/java/com/ssafy/backend/domain/routine/service/RoutineService.java
@@ -3,6 +3,7 @@ package com.ssafy.backend.domain.routine.service;
 import com.ssafy.backend.domain.routine.dto.RoutineDto;
 import com.ssafy.backend.domain.routine.dto.RoutineTrainingRequestDto;
 import com.ssafy.backend.domain.routine.dto.RoutineTrainingResponseDto;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -13,4 +14,6 @@ public interface RoutineService {
     public void updateRoutine(Long routineId, String newTitle);
     public List<RoutineTrainingResponseDto> detailRoutine(Long routineId);
     public void addSet(RoutineTrainingRequestDto requestDto);
+    public void saveRoutineTrainings(Long routineId, List<Long> trainings);
+    public void removeSet(Long routineTrainingListId);
 }
