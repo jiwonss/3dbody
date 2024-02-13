15cd9e0269ae2a8075e17cca516e2a98e1613308
김민욱 <minwookkim@naver.com>
2024-02-14 08:40:24 +0900

    Feat: 3D 작업중

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,12 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router/Router.jsx";
 import "./index.css";
 import { RecoilRoot } from "recoil";
+import { Suspense } from 'react';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RecoilRoot>
-    <RouterProvider router={router} fallbackElement={"로딩중!!!!!"} />
+    <Suspense>
+      <RouterProvider router={router} fallbackElement={"로딩중!!!!!"} />
+    </Suspense>
   </RecoilRoot>
 );
