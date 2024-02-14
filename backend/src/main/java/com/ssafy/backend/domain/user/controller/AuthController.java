dfa86fe5d63e271a3aceb0feb3cf9dac6e85eba6
서지원 <sjs3326@naver.com>
2024-02-14 10:15:15 +0900

    style: User 로그 삭제

--- backend/src/main/java/com/ssafy/backend/domain/user/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/controller/AuthController.java
@@ -31,21 +31,14 @@ public class AuthController {
 
     @PostMapping("/signup")
     public ResponseEntity signup(@RequestBody SignupRequestDto signupRequestDto) {
-        log.info("회원가입 - signupRequestDto : {}", signupRequestDto);
-
         authService.signup(signupRequestDto);
         return ResponseEntity.ok(Response.success());
     }
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody LoginRequestDto loginRequestDto) {
-        log.info("로그인 - loginRequestDto : {}", loginRequestDto);
-
         UserInfoDto userInfoDto = authService.login(loginRequestDto.getEmail(), loginRequestDto.getPassword());
         TokenDto tokenDto = jwtService.issueToken(userInfoDto);
-
-        log.info("로그인 - userInfoDto : {}", userInfoDto);
-        log.info("로그인 - tokenDto : {}", tokenDto);
 
         return ResponseEntity.ok(Response.success(
                 LoginResponseDto.builder()
@@ -59,8 +52,6 @@ public class AuthController {
 
     @PostMapping("/logout")
     public ResponseEntity logout(@RequestHeader("Authorization") String accessToken) {
-        log.info("로그아웃 - accessToken : {}", accessToken);
-
         jwtService.addBlackList(accessToken);
         return ResponseEntity.ok((Response.success(HttpStatus.OK.name(), "")));
 
@@ -68,8 +59,6 @@ public class AuthController {
 
     @PostMapping("/reissue")
     public ResponseEntity reissue(@RequestBody ReissueDto reissueDto) {
-        log.info("토큰 재발급 - reissueDto : {}", reissueDto);
-
         TokenDto tokenDto = authService.reissue(reissueDto.getRefreshToken());
 
         Map<String, Object> map = new HashMap<>();
@@ -81,7 +70,6 @@ public class AuthController {
 
     @GetMapping
     public ResponseEntity duplicateCheckEmail(@RequestParam String email) {
-        log.info("이메일 중복 확인 - email : {}", email);
         if (authService.duplicateCheckEmail(email)) {
             return ResponseEntity.ok(Response.fail("", ""));
         }
--- backend/src/main/java/com/ssafy/backend/domain/user/controller/UserController.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/controller/UserController.java
@@ -24,8 +24,6 @@ public class UserController {
 
     @GetMapping("/{user_id}")
     public ResponseEntity getUserInfo(@PathVariable("user_id") Long userId) {
-        log.info("User 회원 정보 조회 - userId : {}", userId);
-        
         return ResponseEntity.ok(Response.success(userService.getUserInfo(userId)));
     }
 
@@ -36,8 +34,6 @@ public class UserController {
 
     @PostMapping("/{user_id}/password/check")
     public ResponseEntity checkPassword(@PathVariable("user_id") Long userId, @RequestBody PasswordRequestDto passwordRequestDto) {
-        log.info("User 비밀번호 확인 - passwordRequestDto : {}", passwordRequestDto);
-
         if (!userService.checkPassword(userId, passwordRequestDto.getCurrentPassword())) {
             return ResponseEntity.ok(Response.fail(HttpStatus.BAD_REQUEST.name(), "비밀번호가 맞지 않습니다."));
         }
@@ -46,8 +42,6 @@ public class UserController {
 
     @PatchMapping("/{user_id}/password")
     public ResponseEntity changePassword(@PathVariable("user_id") Long userId, @RequestBody PasswordRequestDto passwordRequestDto) {
-        log.info("User 비밀번호 변경 - passwordRequestDto : {}", passwordRequestDto);
-        
         try {
             userService.changePassword(userId, passwordRequestDto);
         } catch (UserException e) {
@@ -58,16 +52,12 @@ public class UserController {
 
     @DeleteMapping("/{user_id}")
     public ResponseEntity withdraw(@PathVariable("user_id") Long userId) {
-        log.info("User 회원탈퇴 - userId : {}", userId);
-
         userService.updateStatus(userId);
         return ResponseEntity.ok(Response.success());
     }
 
     @GetMapping("/{user_id}/nickname")
     public ResponseEntity duplicateCheckNickname(@PathVariable("user_id") Long userId, @RequestParam String nickname) {
-        log.info("User 닉네임 중복 확인 - nickname : {}", nickname);
-
         if (userService.duplicateCheckNickname(nickname)) {
             return ResponseEntity.ok(Response.fail(HttpStatus.BAD_REQUEST.name(), "중복된 닉네임이 존재합니다."));
         }
@@ -75,8 +65,6 @@ public class UserController {
     }
     @PostMapping("/{user_id}/pin/check")
     public ResponseEntity checkPin(@PathVariable("user_id") Long userId, @RequestBody PinRequestDto pinRequestDto) {
-        log.info("User PIN 확인 - pinRequestDto : {}", pinRequestDto);
-
         if (!userService.checkPin(userId, pinRequestDto.getCurrentPin())) {
             return ResponseEntity.ok(Response.fail(HttpStatus.BAD_REQUEST.name(), "PIN이 맞지 않습니다."));
         }
@@ -85,16 +73,12 @@ public class UserController {
 
     @PostMapping("/{user_id}/pin")
     public ResponseEntity createPin(@PathVariable("user_id") Long userId, @RequestBody PinRequestDto pinRequestDto) {
-        log.info("User PIN 생성 - pinRequestDto : {}", pinRequestDto);
-        
         userService.createPin(userId, pinRequestDto);
         return ResponseEntity.ok(Response.success());
     }
 
     @PatchMapping("/{user_id}/pin")
     public ResponseEntity updatePin(@PathVariable("user_id") Long userId, @RequestBody PinRequestDto pinRequestDto) {
-        log.info("User PIN 수정 - pinRequestDto : {}", pinRequestDto);
-        
         try {
             userService.changePin(userId, pinRequestDto);
         } catch (UserException e) {
@@ -105,24 +89,18 @@ public class UserController {
 
     @DeleteMapping("/{user_id}/pin")
     public ResponseEntity deletePin(@PathVariable("user_id") Long userId) {
-        log.info("User PIN 삭제");
-        
         userService.deletePin(userId);
         return ResponseEntity.ok(Response.success());
     }
 
     @PatchMapping("/{user_id}/name")
     public ResponseEntity updateName(@PathVariable("user_id") Long userId, @RequestBody UpdateRequestDto updateRequestDto) {
-        log.info("User 이름 변경 - nickname : {}", updateRequestDto.getNickname());
-
         userService.updateName(userId, updateRequestDto.getName());
         return ResponseEntity.ok(Response.success());
     }
 
     @PatchMapping("/{user_id}/nickname")
     public ResponseEntity updateNickname(@PathVariable("user_id") Long userId, @RequestBody UpdateRequestDto updateRequestDto) {
-        log.info("User 닉네임 변경 - nickname : {}", updateRequestDto.getNickname());
-
         userService.updateNickname(userId, updateRequestDto.getNickname());
         return ResponseEntity.ok(Response.success());
     }
@@ -130,40 +108,30 @@ public class UserController {
 
     @PatchMapping("/{user_id}/gender")
     public ResponseEntity updateGender(@PathVariable("user_id") Long userId, @RequestBody UpdateRequestDto updateRequestDto) {
-        log.info("User 성별 변경 - nickname : {}", updateRequestDto.getGender());
-        
         userService.updateGender(userId, updateRequestDto.getGender());
         return ResponseEntity.ok(Response.success());
     }
 
     @PatchMapping("/{user_id}/height")
     public ResponseEntity updateHeight(@PathVariable("user_id") Long userId, @RequestBody UpdateRequestDto updateRequestDto) {
-        log.info("User 키 변경 - height : {}", updateRequestDto.getHeight());
-
         userService.updateHeight(userId, updateRequestDto.getHeight());
         return ResponseEntity.ok(Response.success());
     }
 
     @PatchMapping("/{user_id}/weight")
     public ResponseEntity updateWeight(@PathVariable("user_id") Long userId, @RequestBody UpdateRequestDto updateRequestDto) {
-        log.info("User 몸무게 변경 - height : {}", updateRequestDto.getWeight());
-
         userService.updateWeight(userId, updateRequestDto.getWeight());
         return ResponseEntity.ok(Response.success());
     }
 
     @PatchMapping("/{user_id}/birth-date")
     public ResponseEntity updateBirthDate(@PathVariable("user_id") Long userId, @RequestBody UpdateRequestDto updateRequestDto) {
-        log.info("User 생년월일 변경 - birthDate : {}", updateRequestDto.getBirthDate());
-
         userService.updateBirthDate(userId, updateRequestDto.getBirthDate());
         return ResponseEntity.ok(Response.success());
     }
 
     @PatchMapping("/{user_id}/profile-image")
     public ResponseEntity updateProfileImage(@PathVariable("user_id") Long userId, @RequestBody UpdateRequestDto updateRequestDto) {
-        log.info("User 프로필이미지 변경 - profileImage : {}", updateRequestDto.getProfileImage());
-
         userService.updateProfileImage(userId, updateRequestDto.getProfileImage());
         return ResponseEntity.ok(Response.success());
     }
